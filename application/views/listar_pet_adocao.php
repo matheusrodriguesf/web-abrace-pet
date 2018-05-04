e530fa07683619a317b705d882d599d25aa3a9a2
IgorGSarmento <igor_gss@hotmail.com>
2018-05-04 17:40:42 -0300

    Pagina de listagem quase finalizada.

--- application/views/listar_pet_adocao.php
+++ application/views/listar_pet_adocao.php
@@ -1,52 +1,39 @@
-<!DOCTYPE html>
-<html>
+<?php $this->load->view('shared/cabecalho'); ?>
 
-	<head>
-	    <meta charset="utf-8" />
-	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	    <title>Page Title</title>
-	    <meta name="viewport" content="width=device-width, initial-scale=1">
-	    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-	    <script src="main.js"></script>
-	</head>
+<div>
+	<div>
+		<h1>Abrace Pets - Adoção</h1>
+	</div>
 
-	<body>
-	    <div>
-			<div>
-				<h1>Abrace Pets - Adoção</h1>
-			</div>
-			<?php if ($this->session->flashdata('error') == TRUE): ?>
-				<p><?php echo $this->session->flashdata('error'); ?></p>
+	<?php if ($this->session->flashdata('error') == TRUE): ?>
+		<p><?php echo $this->session->flashdata('error'); ?></p>
+	<?php endif; ?>
+	<?php if ($this->session->flashdata('success') == TRUE): ?>
+		<p><?php echo $this->session->flashdata('success'); ?></p>
+	<?php endif; ?>
+
+	<table class="table">
+		<thead>
+			<tr>
+			  	<th scope="col">Nome</th>
+			  	<th scope="col">Sexo</th>
+			  	<th scope="col">Descrição</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php if ($pets == FALSE): ?>
+				<tr><td colspan="2">Nenhum pet encontrado</td></tr>
+			<?php else: ?>
+				<?php foreach ($pets as $row): ?>
+					<tr>
+						<td><?= $row['NOMEANIMAL'] ?></td>
+						<td><?= $row['SEXO'] ?></td>
+						<td><?= $row['DESCRICAO_ANIMAL'] ?></td>
+					</tr>
+				<?php endforeach; ?>
 			<?php endif; ?>
-			<?php if ($this->session->flashdata('success') == TRUE): ?>
-				<p><?php echo $this->session->flashdata('success'); ?></p>
-			<?php endif; ?>
+		</tbody>
+	</table>
 
-			<div>
-				<table>
-					<caption>Contatos</caption>
-					<thead>
-						<tr>
-							<th>Nome</th>
-							<th>Sexo</th>
-							<th>Descrição</th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php if ($pets == FALSE): ?>
-							<tr><td colspan="2">Nenhum pet encontrado</td></tr>
-						<?php else: ?>
-							<?php foreach ($pets as $row): ?>
-								<tr>
-									<td><?= $row['NOMEANIMAL'] ?></td>
-									<td><?= $row['SEXO'] ?></td>
-								</tr>
-							<?php endforeach; ?>
-						<?php endif; ?>
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</body>
-</html>
 
+<?php $this->load->view('shared/rodape'); ?>
