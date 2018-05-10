0fb61fe8cd876b1f64aa87bd30e70ea16686f3c5
FelipeMSJ <felipemsoad@gmail.com>
2018-05-10 12:46:54 -0300

    Update quem_somos.php

--- application/views/quem_somos.php
+++ application/views/quem_somos.php
@@ -1,17 +1,66 @@
 <?php $this->load->view('shared/cabecalho'); ?>
 <!-- Page Content -->
+<style>
+	p {
+    	text-align:justify;  
+    	text-justify:auto;
+		text-indent: 5em;
+	}
+</style>
+
 <div class="container">
 			<div class="row">
 				<div class="col-md-12">
             		<h1 class="my-4">
-						Quem Somos
+						Quem Somos 🐾
 					</h1>
 					<div class="media">
-						<img style="float:left; margin:0 15px 10px 0" width="200px" height="100px class="mr-3" alt="Bootstrap Media Preview" src="http://www.professionalrelo.com/wp-content/uploads/faq-pets-960x480.jpg" />
-						<p>
-					  		Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
-			    		</p> 
-                		<br /><br /><br /><br /><br />
+						<img style="float:left; margin:0 15px 10px 0" width="150px" height="150px" class="mr-3" alt="Bootstrap Media Preview" src="http://familia4patas.com.br/wp-content/uploads/um-espaco-de-alimentacao-e-higiene-para-cada-um.png" />
+						<div>
+							<p>
+								Adotar um pet pode ser muito difícil pela dificuldade de divulgação 
+								e de um ambiente que facilite a interação entre quem deseja adotar 
+								ou doar um animal.</br>
+							</p>
+							<p>
+								A Abrace Pets foi originado em março de 2018, oferecendo seu serviço 
+								através do site ou pelo aplicativo de Smartfone. Possui dois objetos
+								principais:
+			    			</p> 
+							<p>
+								1) Facilitar o processo de adoção de animais, possui 
+								a disponibilização de uma plataforma onde pessoas sejam capazes 
+								de anunciar pets, gato e/ou cachorro, e divulgá-lo para poderem 
+								ser adotados. E permite quem deseja adotar encontre 
+								com mais facilidade o pet que tanto deseja.
+							</p>
+							<p>
+								2) Atender pessoas que por algum motivo não irão conseguir cuidar 
+								de seu pet por um curto período de tempo, para isso a Abrace Pet 
+								tem o sistema de Lar Temporário, possibilitando que o dono disponibilize 
+								seu pet para outra pessoa cuidar até que consiga cuidar do animal 
+								novamente.
+							</p>
+							</br> </br></br>
+							<h4>
+								Lei nº 9.605/95
+							</h2>
+							<p>
+								Art. 32: Praticar ato de abuso, maus-tratos, ferir ou mutilar animais silvestres, domésticos ou domesticados, nativos ou exóticos.
+							</p>
+							<p>
+							Pena - detenção, de três meses a um ano, e multa.
+							</p>
+							<h4>
+								Lei nº 2848/40
+							</h2>
+							<p>
+								Art. 171: Estelionato - Infração penal que envolve enganação, golpe, fraude e outras artimanhas ardilosas.
+							</p>
+							<p>
+								Pena - 1 a 5 anos de reclusão, mais o pagamento de multa.
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
